616814da7957929667b21b1174097c4ffd7dc79f
dickv940-star <dickv940@gmail.com>
2026-07-27 18:49:56 +0700

    Create basetool.js

---
+++ js/tools/basetool.js
@@ -0,0 +1,36 @@
+/*
+=========================================================
+SMARTPRINT V5
+Base Tool
+=========================================================
+*/
+
+"use strict";
+
+export default class BaseTool {
+
+    constructor(designer){
+
+        this.designer = designer;
+
+    }
+
+    activate(){}
+
+    deactivate(){}
+
+    pointerDown(){}
+
+    pointerMove(){}
+
+    pointerUp(){}
+
+    keyDown(){}
+
+    keyUp(){}
+
+    wheel(){}
+
+    drawOverlay(){}
+
+}
