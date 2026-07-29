7735da916a3ab937bdc2e62d5460182502426805
dickv940-star <dickv940@gmail.com>
2026-07-29 11:11:56 +0700

    Update pdf.js

--- js/pdf.js
+++ js/pdf.js
@@ -1,33 +1,18 @@
-'use strict';
+"use strict";
 
 const PDFEngine = {
 
     async load(file) {
 
-        try {
+        const arrayBuffer = await file.arrayBuffer();
 
-          const pdfLib = window.pdfjsLib || window['pdfjs-dist/build/pdf'];
+        const pdf = await pdfjsLib.getDocument({
+            data: arrayBuffer
+        }).promise;
 
-if (!pdfLib) {
-    console.error("window.pdfjsLib =", window.pdfjsLib);
-    console.error("window =", window);
-    throw new Error("PDF.js library belum dimuat");
-}
-
-pdfLib.GlobalWorkerOptions.workerSrc =
-'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js';
-
-const pdf = await pdfLib.getDocument({
-    data: arrayBuffer
-}).promise;
-
-            console.log('PDF Pages:', pdf.numPages);
-
-            return pdf;
-
-        } catch (error) {
-            console.error('PDF Engine Error:', error);
-            throw error;
-        }
+        return pdf;
     }
+
 };
+
+window.PDFEngine = PDFEngine;
